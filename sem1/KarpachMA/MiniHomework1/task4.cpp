38eccddbdbbfdd3a3ddc0338d80748958a323c5f
MaxKarpach <127778887+MaxKarpach@users.noreply.github.com>
2024-09-16 00:08:32 +0300

    Update task4.cpp

--- sem1/KarpachMA/MiniHomework1/task4.cpp
+++ sem1/KarpachMA/MiniHomework1/task4.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
-void numberBackwards(int number);
+int numberBackwards(int number);
 int main()
 {
     int number = 0;
     std::cout << "Введите число" << std::endl;
     std::cin >> number;
-    numberBackwards(number);
+    std::cout << "Число наоборот это ";
+    std::cout << numberBackwards(number);
 }
-void numberBackwards(int number){
-    while (number > 0){
-        std::cout << number % 10;
+int numberBackwards(int number){
+    int numberLength = 0;
+    int numberDublciate = number;
+    int transformedNumber = 0;
+    while (number > 0)
+    {
+        numberLength++;
         number /= 10;
     }
+     while (numberDublciate > 0)
+    {
+        transformedNumber += ((pow(10, numberLength - 1)) * (numberDublciate % 10));
+        numberDublciate /= 10;
+        numberLength--;
+    }
+
+    return transformedNumber;
 }
