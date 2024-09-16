3ba7304e8de42cc7e4a50cbc16bc4fd886736bd9
Максим Евдокимов <maksimevdokimov@AzuleBook.local>
2024-09-16 15:16:09 +0300

    First homework 1-4

---
+++ sem1/EvdokimovMA/task1.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Максим Евдокимов on 16.09.2024.
+//
+
+float squareArea(const float side) {
+    return side * side;
+}
---
+++ sem1/EvdokimovMA/task2.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Максим Евдокимов on 16.09.2024.
+//
+
+float trapezoidzArea(const float base0, const float base1, const float height) {
+    return (base0 + base1) * 0.5f * height;
+}
