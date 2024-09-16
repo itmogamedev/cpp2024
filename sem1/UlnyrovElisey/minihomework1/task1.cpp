4b0ed7b3adb02dbe9ddd022eef99540c694447b9
Ulnyrov <fc.unstoppable@gmail.com>
2024-09-16 20:53:57 +0300

    minihomework1 (Ulnyrov Elisey)

---
+++ sem1/UlnyrovElisey/minihomework1/task1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+int AreaOfSquare(int SideOfSquare)
+{
+	return SideOfSquare * SideOfSquare;
+}
+
+int main()
+{
+	int x = 0;
+	std::cout << "x = ";
+	std::cin >> x;
+	int S = AreaOfSquare(x);
+	std::cout << "S = ";
+	std::cout << S;
+	return 0;
+}
---
+++ sem1/UlnyrovElisey/minihomework1/task3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+int main() {
+	int N = 0;
+	int first = 0;
+	int second = 1;
+	int next = 0;
+	int count = 2;
+	
+	std::cout << "N = ";
+	std::cin >> N;	
+	
+	if (N <= 0)
+	{
+		std::cout << "";
+	}
+	else if (N == 1)
+	{
+		std::cout << first;
+	}
+	else
+	{
+		std::cout << first << " " << second;
+	}
+	
+		while (count < N)
+	{
+			next = first + second;
+			first = second;
+			second = next;
+			count++;
+			std::cout << " " << next;
+	}
+		return 0;
+}
