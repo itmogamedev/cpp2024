499cf03d0a50b9f8023041ec00130662904ef0e7
Роман Литвинчук <roman67877@gmail.com>
2024-09-18 13:13:57 +0300

    Create MiniHomeWork1 (Roman Litvinchuk)

---
+++ sem1/RomanLitvinchuk/MiniHomeWork1/Task4.cpp
@@ -0,0 +1,22 @@
+﻿# include <iostream>
+# include <math.h>
+
+int main()
+{
+	int number = 0;
+	int ReverseNumber = 0;
+	int digit = -1;
+	std::cout << "Number?";
+	std::cin >> number;
+	int storage = number;
+	while (storage != 0) {
+		storage = storage / 10;
+		digit += 1;
+	}
+	while (number != 0) {
+		ReverseNumber += number % 10 * pow(10, digit);
+		number = number / 10;
+		digit -= 1;
+	}
+	std::cout << "Reverse number=" << ReverseNumber;
+}
