50461e71727813311f2a20bdd0519924e9cc64c2
Arseniy Skovpen <arseniy20172017@mail.ru>
2024-09-18 15:03:07 +0300

    Create minihomework1 (Arseniy Skovpen)

---
+++ sem1/SkovpenArseniy/MiniHomeWork1/task 1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int side, square;
+	std::cout << "Введите сторону квадрата:";
+	std::cin >> side;
+	square= pow(side, 2);
+	std::cout << "Площадь равна : " << square;
+
+
+
+	return 0;
+}
+
+
