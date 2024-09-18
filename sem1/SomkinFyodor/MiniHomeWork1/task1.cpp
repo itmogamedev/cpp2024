610c40a70d61c60407f860aec67079e7ff635155
Фёдор <somkinf@gmail.com>
2024-09-18 13:44:14 +0300

    MiniHomeWork1

---
+++ sem1/SomkinFyodor/MiniHomeWork1/task1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int ploshad(int height, int width) {
+    int squarearea;
+    squarearea = height * width;
+    return squarearea;
+
+}
+
+int main()
+{
+    int height, width, squarearea;
+    std::cin >> height >> width;
+    squarearea = ploshad(height, width);
+    std::cout << squarearea;
+}
+
