8dfd82fd11c3e4ee8fd1a11e753ffad1a818c5fb
Анастасия Михайлова <anatirockstar@gmail.com>
2024-09-18 23:54:03 +0300

    Create MiniHomeWork1 (Mikhaylova Anastasia)

---
+++ sem1/MikhaylovaAnastasia/MiniHomeWork1/task1.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+
+double square(double a) {
+    return a * a;
+}
+
+int main()
+{ 
+    setlocale(LC_ALL, "rus");
+    double a;
+    std::cin>>a;
+    std::cout << "Площадь квадрата:" << square(a);
+}
+
