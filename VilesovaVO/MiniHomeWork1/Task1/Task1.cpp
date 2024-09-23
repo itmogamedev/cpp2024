b6226cdb7107c19b21fec3de683c5fc983d0f910
VilesovaVO <Lerochka03012414@gmail.com>
2024-09-23 22:30:49 +0300

    Add files via upload

---
+++ VilesovaVO/MiniHomeWork1/Task1/Task1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+int main()
+{
+    int s, a;
+    std::cout << "Enter side of square :" << std::endl;
+    std::cin >> a;
+    s = a * a;
+    std::cout << "Side of square " << a << std::endl;
+    std::cout << "Square " << s << std::endl;
+
+}
