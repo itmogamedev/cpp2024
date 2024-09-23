abc783b072301d6d6fb35d4181edd83c089d83d3
spygun62766 <spygun62766@gmail.com>
2024-09-23 16:52:00 +0300

    Solution

---
+++ MiniHomework 1/task 2/task 2/task 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    int H = 0;
+    int a = 0;
+    int b = 0;
+    std::cout << "H: ";
+    std::cin >> H ;
+    std::cout << "A:";
+    std::cin >> a ;
+    std::cout << "B:";
+    std::cin >> b ;
+    std::cout << ((a + b) / 2) * H ;
+    return 0;
+}
---
+++ MiniHomework 1/task 3/task 3/task 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n = 0;
+    int a = 0;
+    int g = 0;
+    int b = 1;
+    std::cout << "N:";
+    std::cin >> n;
+    std::cout << b;
+    n = n - 1;
+    while (n > 0)
+    {
+        std::cout << a + b;
+        g = b;
+        b = a + b;
+        a = g;
+        n = n-1;
+    }
+
+}
