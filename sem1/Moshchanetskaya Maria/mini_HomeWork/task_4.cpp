98de97d8d1c59933392976e2ba8545d54045895c
Luckymilkk <masha27052006@gmail.com>
2024-09-23 18:29:43 +0300

    Moshchanetskaya Maria Mini HW 1

---
+++ sem1/Moshchanetskaya Maria/mini_HomeWork/task_4.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int number;
+    int new_number = 0;
+    std::cout << "введите натуральное число" << std::endl;
+    std::cin >> number;
+    while (number > 0)
+    {
+        new_number = new_number * 10 + number % 10;
+        number = number / 10;
+    }
+    std::cout << new_number;
+
+}
