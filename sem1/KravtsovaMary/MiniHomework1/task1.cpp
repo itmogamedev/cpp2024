565a4f684d591c9948fa5330f8951ae7a71e54dd
Кравцова Мария <kravtsovamariya45@gmail.com>
2024-09-23 19:42:28 +0300

    Create HomeWork1 (Kravtsova Mary)

--- sem1/KravtsovaMary/MiniHomework1/task1.cpp
+++ sem1/KravtsovaMary/MiniHomework1/task1.cpp
@@ -3,7 +3,7 @@ int sqs(int a) {
 	return a * a;
 }
 int main() {
-	int side = 0;
+	int side;
 	std::cin >> side;
 	int res = sqs(side);
 	std::cout << res;
--- sem1/KravtsovaMary/MiniHomework1/task2.cpp
+++ sem1/KravtsovaMary/MiniHomework1/task2.cpp
@@ -3,9 +3,7 @@ int strap(int a, int b, int h) {
 	return (a + b) / 2 * h;
 }
 int main() {
-	int side1 = 0;
-	int side2 = 0;
-	int hight = 0;
+	int side1, side2, hight;
 	std::cin >> side1 >> side2 >> hight;
 	int res = strap(side1, side2, hight);
 	std::cout << res;
