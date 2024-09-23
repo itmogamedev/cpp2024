cded64198a60a1d83cdc3cb3cc9ac754505af7e8
Muhamed Muratdurdyyev <vaulinikita3@gmail.com>
2024-09-23 14:41:57 +0300

    tasks 1-4

---
+++ sem1/Muhamed Muratdurdyyev/dz3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath> 
+
+int main()
+{ 
+    int cup;
+    
+    std::cout<<"Side of a square: ";
+    std::cin>> cup ;
+    
+    std::cout<< "Area:"<<pow (cup,2);
+    
+    return 0;
+}
