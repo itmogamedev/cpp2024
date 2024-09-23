bde44e4b95c5fd731afd6a195ffc07d027382ef3
Кузнецов Григорий <grisakuznecov90934@gmail.com>
2024-09-23 19:35:31 +0300

    Create minihomework1(Kuznecov Grigory

---
+++ sem1/KuznecovGrigory/MiniHomeWork1/HW2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+int TrapezoidArea(int basis1, int basis2, int height)
+{
+	return (basis1 + basis2) * height / 2;
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int TrapezoidSide1, TrapezoidSide2, TrapezoidHeight;
+	std::cout << "Введите длину оснований и высоту трапеции:";
+	std::cin >> TrapezoidSide1 >> TrapezoidSide2 >> TrapezoidHeight;
+	std::cout << "Площадь трапеции:" << TrapezoidArea(TrapezoidSide1, TrapezoidSide2, TrapezoidHeight) << '\n';
+	return 0;
+}
