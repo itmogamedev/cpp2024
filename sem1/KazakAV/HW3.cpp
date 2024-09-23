310e1f89055ac839080e5b66dd722dcf8410675d
Ангелина Казак <anna.kaza.05@gmail.com>
2024-09-23 19:09:28 +0300

    upd hw3

--- sem1/KazakAV/HW3.cpp
+++ sem1/KazakAV/HW3.cpp
@@ -5,10 +5,15 @@ int main() {
 	int i0 = 0;
 	int i1 = 1;
 	std::cin >> number;
-	std::cout << "1 ";
-	for (int i = 0; i < number; i++) {
-		std::cout << (i1 + i0) << " ";
-		i1 = i0 + i1;
-		i0 = i1 - i0;
+	if (number == 0) {
+		std::cout << "wrong";
+	}
+	else{
+		std::cout << "1 ";
+		for (int i = 0; i < number - 1; i++) {
+			std::cout << (i1 + i0) << " ";
+			i1 = i0 + i1;
+			i0 = i1 - i0;
+		}
 	}
 }
