9de9cf4cb218b274cc8bc88f7c718bb8b46562a1
Storm611 <loni48334@gmail.com>
2024-09-24 21:42:25 +0300

    BolonevIAMiniHomeWorktask2.cpp

--- sem1/BolonevIvan/MiniHomeWork1/BolonevIAMiniHomeWorktask2.cpp
+++ sem1/BolonevIvan/MiniHomeWork1/BolonevIAMiniHomeWorktask2.cpp
@@ -1,14 +1,19 @@
-﻿#include <iostream>
+#include <iostream>
+void trapezoid(float a,float b,float c)
+{
+    std::cout << "Площадь трапеции = "<< (a +b)* c/2 << std::endl;
+}
 
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	double a, b, c;
-	std::cout << "Введите первое основание трапеции" << std::endl;
-	std::cin >> a;
-	std::cout << "Введите второе основание трапеции" << std::endl;
-	std::cin >> b;
-	std::cout << "Введите высоту трапеции" << std::endl;
-	std::cin >> c;
-	std::cout<< (a + b) / 2 * c; 
+    float a,b,c;
+    setlocale(LC_ALL, "ru");
+    std::cout << "Введите длину первого основания" << std::endl;
+    std::cin >> a;
+    std::cout << "Введите длину второго основания" << std::endl;
+    std::cin >> b;
+    std::cout << "Введите длину высоты" << std::endl;
+    std::cin >> c;
+    trapezoid(a, b, c);
+    return 0;
 }
