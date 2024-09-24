06a246e581a63620cc6052881a3d9eb711b4876b
NagornevV <91148305+NagornevV@users.noreply.github.com>
2024-09-25 00:20:52 +0300

    task_2.cpp

--- sem1/NagornevVladimir/MiniHomeWork1/task_2.cpp
+++ sem1/NagornevVladimir/MiniHomeWork1/task_2.cpp
@@ -1,15 +1,17 @@
-﻿#include <iostream>
+#include <iostream>
 
-int func_area_square(int a, int b) {
-	std::cout << "Area square = " << a * b;
-	return a * b;
+float func_area_trapeze(float a, float b, float c) {
+	std::cout << "Area trapeze = " << (a + b) / 2 * c;
+	return 1;
 }
 
 int main() {
-	int side_1, side_2;
+	float side_1, side_2, hight;
 	std::cout << "Enter first side: ";
 	std::cin >> side_1;
 	std::cout << "Enter second side: ";
 	std::cin >> side_2;
-	func_area_square(side_1, side_2);
+	std::cout << "Enter hight: ";
+	std::cin >> hight;
+	func_area_trapeze(side_1, side_2, hight);
 }
