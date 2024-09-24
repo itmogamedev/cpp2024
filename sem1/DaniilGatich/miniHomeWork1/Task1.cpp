afd72e3a93d925dab17fbac9908e6309fa2e85bc
DaniilGatich <gatich77@gmail.com>
2024-09-24 16:53:52 +0300

    ДЗ - Гатич

---
+++ sem1/DaniilGatich/miniHomeWork1/Task1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+float square(float side)
+{
+    std::cout << "Enter side of your quadrant:";
+    std::cin >> side;
+    std::cout << "Square of your quadrant is - " << side * side;
+    return 0;
+}
+
+int main()
+{
+    float usersSide = 0;
+    square(usersSide);
+}
+
+
