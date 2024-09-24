872a3f7c808ecc13dc935dcef58b5fc6d808653e
Evdokimova <4186215@gmail.com>
2024-09-24 16:49:12 +0300

    Task1

---
+++ sem1/KondyukovKR/Evdokimova Varya/Task2/Task2/Task2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	int h;
+	cout << "Enter the length of the trapezoid bases:";
+	cin >> a >> b;
+	cout << "Enter the length of the trapezoid height:";
+	cin >> h;
+	cout << (a + b) / 2 * h;
+	return 0;
+}
---
+++ sem1/KondyukovKR/Evdokimova Varya/Task3/Task3/Task3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 0, b = 1, c, n, cnst, i;
+    cout << "Enter number of numbers: ";
+    cin >> n;
+    for (i = 1; i <= n; i++) 
+    {
+        if (i == 1)  // first numb
+            c = 0;
+        else if (i == 2 or i == 3)
+            c = 1;
+        else
+        {
+            cnst = a;
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        if (i == n) // last number
+            cout << c;
+        else
+            cout << c << ", "; // str
+    }
+    cout << endl;
+    return 0;
+}
