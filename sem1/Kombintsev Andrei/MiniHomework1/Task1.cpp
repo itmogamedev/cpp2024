87ef9b0015768afc769bfa385ab2d9d89f613c21
Kombintsev <forfesed@gmail.com>
2024-09-24 19:37:53 +0300

    Create minihomework1 (Kombintsev Andrei)

---
+++ sem1/Kombintsev Andrei/MiniHomework1/Task1.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+int main()
+{
+	int side = 0;
+	std::cout << "Enter a side of square: ";
+	std::cin >> side;
+	std::cout << "area of square: " << side*side;
+}
---
+++ sem1/Kombintsev Andrei/MiniHomework1/Task2.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter side of trapezoid";
+    float a = 0;
+    float b = 0;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << "Enter trapezoid height";
+    float h = 0;
+    std::cin >> h;
+    std::cout << "Area of trapezoid" << (a + b) / 2.0 * h;
+}
+
