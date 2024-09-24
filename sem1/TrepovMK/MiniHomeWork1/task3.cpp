53f5328aa82c41b72989f0f94117f5c2c756bfc6
TrepovMK <fixik243@gmail.com>
2024-09-24 18:35:58 +0300

    did homework TrepovMK

---
+++ sem1/TrepovMK/MiniHomeWork1/task3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    int num;
+    int x = 1;
+    int n;
+    int y = 0;
+    std::cout << "Enter number of Fibonacсi numbers";
+    std::cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        if (i == 0) 
+        {
+            std::cout << " " << x;
+        }
+        else
+        {
+            std::cout << " " << x + y;
+            n = x;
+            x = x + y;
+            y = n;
+        }
+        
+
+    }
+}
