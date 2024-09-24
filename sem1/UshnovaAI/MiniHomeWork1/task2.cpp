6b57d992f4e68dd9e730c13df1146bbcda2f9868
AnnaUshnova <anna.ushnova@gmail.com>
2024-09-24 19:30:40 +0300

    miniHomeWork1 (Anna Ushnova)

---
+++ sem1/UshnovaAI/MiniHomeWork1/task2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int areaTrapezoid(int base1, int base2, int height) {
+	return (base1+base2)*height/2;
+}
+
+int main() {
+	int base1 = 4;
+	int base2 = 2;
+	int height = 2;
+	std::cout << "The area of ​​the strapezium is " << areaTrapezoid(base1, base2, height) << std::endl;
+}
