580e7182b70ba79de2d79e6d63bc4ef04a1418b3
Egor Reshetnikov <158984685+Elvor12@users.noreply.github.com>
2024-09-29 17:47:13 +0300

    Task3.cpp
    Changed task

--- sem1/ReshetnikovEgor/MiniHomeWork1/Task3.cpp
+++ sem1/ReshetnikovEgor/MiniHomeWork1/Task3.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
-
-void fibonachi() {
-	int last_numb;
-	std::cin >> last_numb;
+void Fibonachi(int last_numb) {
 	int rememb_numbers[3]{ 0, 1, 1 };
 	for (int i = 0; i < last_numb; i++) {
 		std::cout << rememb_numbers[0] << ' ';
@@ -11,4 +8,12 @@ void fibonachi() {
 		rememb_numbers[1] = rememb_numbers[0] + rememb_numbers[1];
 		rememb_numbers[0] = rememb_numbers[2];
 	}
+}
+int main() {
+	setlocate(LS_ALL, "Russian")
+	int numb_fibon;
+	std::cout << "Введите число для последовательности фибоначи: ";
+	std::cin >> numb_fibon;
+	std::cout << "Последовательнось: ";
+	Fibonachi(numb_fibon);
 }
