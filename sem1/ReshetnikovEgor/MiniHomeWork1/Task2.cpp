6c580baf365378583423da0b0ffeef01d1e278de
Egor Reshetnikov <158984685+Elvor12@users.noreply.github.com>
2024-09-29 17:58:08 +0300

    Task2.cpp

--- sem1/ReshetnikovEgor/MiniHomeWork1/Task2.cpp
+++ sem1/ReshetnikovEgor/MiniHomeWork1/Task2.cpp
@@ -4,7 +4,7 @@ int Trapez(int base1, int base2, int height) {
 	return (base1 + base2) / 2 * height;
 }
 int main() {
-	setlocate(LS_ALL, "Russian")
+	setlocale(LC_ALL, "Russian");
 	int input_1, input_2, input_3;
 	td::cout << "Введите нижнее и верхнее основание трапеции: ";
 	std::cin >>  input_1 >> input_2;
