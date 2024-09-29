a539a5b16565d0cb5738ae09b1a352c6834b9096
Egor Reshetnikov <158984685+Elvor12@users.noreply.github.com>
2024-09-29 17:31:14 +0300

    Task1.cpp
    Changed task

--- sem1/ReshetnikovEgor/MiniHomeWork1/Task1.cpp
+++ sem1/ReshetnikovEgor/MiniHomeWork1/Task1.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
-int square(int numb) {
+int Square(int numb) {
 	return numb * numb;
+}
+
+int main() {
+	setlocate(LS_ALL,"Russian");
+	int input;
+	std::cout << "Введите сторону квадрата: ";
+	std::cin >>  input;
+	std::cout <<  "Площадь квадрата равна: " << Square(input) << std::endl;
 }
