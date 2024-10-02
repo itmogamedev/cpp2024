230332835c90a909d6e22d4091083bcdc2774a4d
Кузнецов Григорий <grisakuznecov90934@gmail.com>
2024-10-02 17:22:53 +0300

    Create minihomework2 (Kuznecov Grigory)

---
+++ sem1/KuznecovGrigory/MiniHomeWork2/mHW2(task1).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+int OctConversion(int number)
+{
+	std::cout << std::oct << number;
+	return 0;
+}
+int HexConversion(int number)
+{
+	std::cout << std::hex << number;
+	return 0;
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int Number = 0, Choose = 0;
+	std::cout << "Выберите в какую систему счисления нужно перевести ваше число:" << '\n';
+	std::cout << "1. 8 система счисления:" << '\n' << "2. 16 система счисления:" << '\n' << "3. Выйти из программы:" << '\n';
+	std::cin >> Choose;
+	switch(Choose)
+	{
+	case 1:
+		std::cout << "Введите ваше число в 10 системе счисления:" << '\n';
+		std::cin >> Number;
+		OctConversion(Number);
+		break;
+	case 2:
+		std::cout << "Введите ваше число в 10 системе счисления:" << '\n';
+		std::cin >> Number;
+		HexConversion(Number);
+		break;
+	case 3:
+		break;
+	default:
+		std::cout << "Можно ввести только цифры 1, 2 и 3";
+	}
+}
---
+++ sem1/KuznecovGrigory/MiniHomeWork2/mHW2(task2).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int number;
+	std::cout << "Введите любое натуральное число кроме 0 и 1" << '\n';
+	std::cin >> number;
+	while (number != 1)
+	{
+		if (number % 2 == 0)
+		{
+			number /= 2;
+		}
+		else
+		{
+			number = (number * 3 + 1) / 2;
+		}
+	}
+	std::cout << "Гипотеза подтверждена";
+
+}
