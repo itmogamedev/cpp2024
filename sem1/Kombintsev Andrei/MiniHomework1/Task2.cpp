ce8a282d4bb7c44fd594e5069d067fe21c7122f5
Andrei Kombintsev <forfesed@gmail.com>
2024-10-02 10:31:53 +0300

    Task2.cpp
    Забыл закомитить while and if - это защита от дурака чтобы не было квадрата

--- sem1/Kombintsev Andrei/MiniHomework1/Task2.cpp
+++ sem1/Kombintsev Andrei/MiniHomework1/Task2.cpp
@@ -3,14 +3,25 @@
 
 int main()
 {
-    std::cout << "Enter side of trapezoid";
-    float a = 0;
-    float b = 0;
-    std::cin >> a;
-    std::cin >> b;
-    std::cout << "Enter trapezoid height";
-    float h = 0;
-    std::cin >> h;
+    float a = 0;  //Одно из оснований трапеции
+    float b = 0;  //Другое основание трапеции
+    float h = 0; //высота 
+    int i = 0;
+    while (i == 0) 
+    {
+        std::cout << "Enter sides of trapezoid";
+       
+        std::cin >> a;
+        std::cin >> b;
+        std::cout << "Enter trapezoid height";
+        
+        std::cin >> h;
+        if (a != b)
+        {
+            i = 1;
+        }
+        else std::cout << "ERROR\n";
+    }
     std::cout << "Area of trapezoid" << (a + b) / 2.0 * h;
 }
 
