a3faf74cdf8a3d2b54230b0a9e343ed661bbffea
Fedor Sobolev <fedor.sobolev1@gmail.com>
2024-10-02 16:28:48 +0300

    Task №3.cpp

--- sem1/Fedor Sobolev/Mini Homework №1/Task №3.cpp
+++ sem1/Fedor Sobolev/Mini Homework №1/Task №3.cpp
@@ -1,19 +1,16 @@
-﻿
-#include <iostream>
-using std::cout;
-using std::cin;
+﻿#include <iostream>
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
     int firstnumber = 0, secondnumber = 1, thirdnumber, amount, counter = 0;
-    cout << "Введите количество элементов числа Фибоначчи:";
-    cin >> amount;
-    cout << firstnumber << " " << secondnumber << " ";
+    std::cout << "Введите количество элементов числа Фибоначчи:";
+    std::cin >> amount;
+    std::cout << firstnumber << " " << secondnumber << " ";
     for (counter; amount - 1 > counter; ++counter) {
         thirdnumber = firstnumber + secondnumber;
         firstnumber = secondnumber;
         secondnumber = thirdnumber;
-        cout << thirdnumber << " ";
+        std::cout << thirdnumber << " ";
     }
 }
