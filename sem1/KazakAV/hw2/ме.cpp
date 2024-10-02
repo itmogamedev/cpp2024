3e195d2eaba4c7d5835b4dda7632080fadb5fba3
Ангелина Казак <anna.kaza.05@gmail.com>
2024-10-02 14:05:00 +0300

    hw2

---
+++ sem1/KazakAV/hw2/ме.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+	int n = 0;
+	std::cin >> n;
+	std::cout << std::oct << n << std::endl;
+	std::cout << std::hex << n;
+}
---
+++ sem1/KazakAV/hw2/ме2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	int n = 0;
+	std::cin >> n;
+	while (n != 1) {
+		if (n % 2 == 0) {
+			n = n / 2;
+		}
+		else {
+			n = ((n * 3) + 1) / 2;
+		}
+	}
+	if (n == 1) {
+		std::cout << "True";
+	}
+}
