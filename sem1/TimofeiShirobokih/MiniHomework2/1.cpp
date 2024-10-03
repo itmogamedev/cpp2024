142c2adab68c9a2161f4bccfc2255ad12a3b208a
ShirobokihTimofei <shirobokihtim@gmail.com>
2024-10-03 12:58:23 +0300

    Second minihomework

---
+++ sem1/TimofeiShirobokih/MiniHomework2/1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    int num;
+    std::string eight, sixteen = "";
+    std::cout << "Enter number to convert in eight and sixteen system: ";
+    std::cin >> num;
+    for (int x = num; x > 0;) {
+        eight = std::to_string(x % 8) + eight;
+        x /= 8;
+    }
+    for (int x = num; x > 0;) {
+        if (x % 16 > 9) {
+            switch (x % 16) {
+            case 10:
+                sixteen += "A";
+            case 11:
+                sixteen += "B";
+            case 12:
+                sixteen += "C";
+            case 13:
+                sixteen += "D";
+            case 14:
+                sixteen += "E";
+            case 15:
+                sixteen += "F";
+            }
+        }
+        else {
+            sixteen = std::to_string(x % 16) + sixteen;
+        }
+        x /= 16;
+    }
+    std::cout << "Your number in eight system: " << eight << std::endl << "Your number in sixteen system: " << sixteen;
+    return 0;
+}
---
+++ sem1/TimofeiShirobokih/MiniHomework2/2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int num;
+    std::cout << "Enter number to use it on Syracuse hypothesis: ";
+    std::cin >> num;
+    while (num != 1) {
+        if ((num % 2) == 1) {
+            num = num * 3 + 1;
+        }
+        num /= 2;
+        std::cout << "Current number: " << num << std::endl;
+    }
+    std::cout << "Hypothesis is correct";
+    return 0;
+}
