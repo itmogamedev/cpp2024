64c5e0e57f55eef36ed144832100def68dea3003
SinRoma <spygun62766@gmail.com>
2024-10-04 19:57:55 +0300

    Add files via upload

---
+++ task 1/task 1/task 1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n = 0;
+    std::cout << "N:";
+    std::cin >> n;
+    std::cout << "8: ";
+    std::cout << std::oct << n;
+    std::cout << "  16: ";
+    std::cout << std::hex << n;
+}
