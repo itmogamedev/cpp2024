53386a0ddf9c8ee08ccade979040d64e734a050d
unknown <anna.volkoff@icloud.com>
2024-10-05 13:52:36 +0300

    homework2

---
+++ sem1/VolkovaAP/hw2.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+	int number;
+	std::cin >> number;
+	while (number != 1) {
+		if (number % 2 == 0) {
+			number /= 2;
+		}
+		else {
+			number = (number * 3 + 1) / 2;
+		}
+	}
+	if (number == 1) {
+		std::cout << "True\n";
+	}
+}
