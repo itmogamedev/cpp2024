65b26b0c29e89cb310a5dd38b703c46f73dc1214
Evdokimova Anna <ivanevdokimov802@gmail.com>
2024-10-05 15:40:28 +0300

    Create MiniHomeWork2 (Evdokimova Anna)

--- sem1/EvdokimovaAnna/MiniHomeWork1/task2.cpp
+++ sem1/EvdokimovaAnna/MiniHomeWork1/task2.cpp
@@ -2,20 +2,14 @@
 
 using std::cout;
 using std::cin;
-using std::endl;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
 	int a;
 	int b;
 	int h;
-	cout << "Сторона 1"<<endl;
 	cin >> a;
-	cout << "Сторона 2"<<endl;
 	cin >> b;
-	cout << "Высота"<<endl;
 	cin >> h;
-	cout << "Площадь трапеции " << endl;
 	cout << (a + b) / 2 * h;
 }
--- sem1/EvdokimovaAnna/MiniHomeWork1/task3.cpp
+++ sem1/EvdokimovaAnna/MiniHomeWork1/task3.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 using std::cout;
 using std::cin;
-using std::endl;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
 	int a = 0; int b = 1; int c; int x;
-	cout << "Введите количество элементов" << endl;
 	cin >> x;
 	cout << 1 << "";
 	for (int i = 0; i < x - 1; ++i) {
