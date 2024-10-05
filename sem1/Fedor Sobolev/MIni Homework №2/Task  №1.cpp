e2070a2e247c1482b53f5128a348f5720a2bed61
Fedor Sobolev <fedor.sobolev1@gmail.com>
2024-10-05 19:26:34 +0300

    Mini Homework №2 (Sobolev Fedor)

---
+++ sem1/Fedor Sobolev/MIni Homework №2/Task  №1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+void decimaltohexadecimal(int decimal) {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Шестнадцатеричное значение: " << std::hex << std::uppercase << decimal << std::endl;
+}
+
+void decimaltooctal(int decimal) {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Восьмеричное значение: " << std::oct << decimal << std::endl;
+}
+
+int main() {
+    int decimal;
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите десятичное число: ";
+    std::cin >> decimal;
+
+    decimaltohexadecimal(decimal);
+    decimaltooctal(decimal);
+
+    return 0;
+}
