549e278688acdc88533028f3ab8b6ee5821e4a68
unknown <anna.volkoff@icloud.com>
2024-10-05 14:06:28 +0300

    homework2

--- sem1/VolkovaAP/hw1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-	int storona; 
-	std::cin >> storona;
-	std::cout << storona * storona;
-}
--- sem1/VolkovaAP/hw2.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-	int a, b, h;
-	std::cin >> a >> b >> h;
-	std::cout << 0.5 * (a + b) * h;
-}
