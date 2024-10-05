44c3eff2561e111d3dbfa54f98796b5091dcdd70
IlyaKruk <ikruk2354@gmail.com>
2024-10-05 19:06:45 +0300

    минидомашка2

---
+++ sem1/IlyaKruk/MiniHomeWork2/task2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    if (a > 1) {
+        while (a > 1) {
+            if (a % 2 == 0) {
+                a = a / 2;
+            }
+            else {
+                a = (a * 3 + 1) / 2;
+            }
+        }
+        cout << a;
+    }
+    else {
+        a = (a * 3 + 1) / 2;
+        while (a > 1) {
+            if (a % 2 == 0) {
+                a = a / 2;
+            }
+            else {
+                a = (a * 3 + 1) / 2;
+            }
+        }
+        cout << a;
+    }
+}
