8a11f23223db6c51814223078eb5d968bfd4fa61
TrepovMK <fixik243@gmail.com>
2024-10-06 19:11:23 +0300

    MiniHW2 done

---
+++ sem1/TrepovMK/MiniHomeWork2/task1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    int i;
+    std::cout << "Enter the number. The first number will be in octal system and second number will be in hexadecimal system ";
+    std::cin >> i;
+    std::cout << std::oct << i << std::endl;
+    std::cout << std::hex << i;
+}
