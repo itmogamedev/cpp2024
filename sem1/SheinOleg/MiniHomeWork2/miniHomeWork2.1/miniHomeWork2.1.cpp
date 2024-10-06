1191e795472962fb51b305ebdfb7d794d4014c79
Shein Oleg <Oleg-40-50@yandex.ru>
2024-10-06 15:50:55 +0300

    minihomework2 (Shein Oleg)

---
+++ sem1/SheinOleg/MiniHomeWork2/miniHomeWork2.1/miniHomeWork2.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	while (true)
+	{
+		long long int number = 0;
+		std::cout << "Введите число для перевода: ";
+		std::cin >> number;
+		if (number >= 0)
+		{
+			std::cout << "Число в восьмиричной системе: " << std::oct << number << std::endl;
+			std::cout << "Число в шестнадцатеричной системе: " << std::hex << number << std::endl << std::endl;
+		}
+		else
+		{
+			std::cout << "Ошибка. Введите положительное число или 0";
+		}
+	}
+}
