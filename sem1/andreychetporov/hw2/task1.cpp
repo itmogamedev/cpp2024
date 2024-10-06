8c76fd76b8a5fde8ddd0f3d7dd9152abe69bfac1
Андрей Четпоров <achetporov@gmail.com>
2024-10-06 12:04:02 +0300

    hw2

---
+++ sem1/andreychetporov/hw2/task1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int number;
+    std::cout << "введите число в десятичной ";
+    std::cin >> number;
+    std::cout << "число в восьмеричной: " << std::oct << number << '\n';
+    std::cout << "число в шестнадцатиричной: " << std::hex << number;
+}
