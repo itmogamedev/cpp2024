9b7ffd50d50450840dd785a892159d935959630c
Polina Shumilina <polshum702@gmail.com>
2024-10-06 22:15:39 +0300

    Create minihomework2 (Shumilina Polina)

---
+++ sem1/PolinaShumilina/MiniHomeWork2/Task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n;
+	std::cout << "Enter the number - ";
+	std::cin >> n;
+	int x;
+	std::cout << "Enter the number system: 16 or 8 - ";
+	std::cin >> x;
+	if (x == 16) {
+		std::cout << std::hex << n;
+	}
+	if (x == 8) {
+		std::cout << std::oct << n;
+	}
+	if (x != 16 and x != 8) {
+		std::cout << "we can't do anything(((";
+	}
+}
