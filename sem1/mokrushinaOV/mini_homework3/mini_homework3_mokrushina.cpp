2a51872dabc4a16878c4f65de51291d91a3299e5
zhenya shur <zhenforshur@gmail.com>
2024-11-07 22:11:15 +0300

    THE TRUE ONE mini homework3 by mokrushina
    я слегка запуталась в гитхабе, так что где-то может быть ещё одна mhw3, она не настоящая!

--- sem1/mokrushinaOV/mini_homework3/mini_homework3_mokrushina.cpp
+++ sem1/mokrushinaOV/mini_homework3/mini_homework3_mokrushina.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <cstdio>
 #include <string>
+#include <sstream>
+
 
 std::vector<std::string> split(std::string str_in, std::string str_to_split_by) {
 
@@ -14,24 +16,22 @@ std::vector<std::string> split(std::string str_in, std::string str_to_split_by) 
     while ((pos = str_in.find(str_to_split_by)) != std::string::npos) {
         token = str_in.substr(0, pos);
         tokens.push_back(token);
-        str_in.erase(0, pos + 1);
+        str_in.erase(0, pos + str_to_split_by.length());
     }
+
     tokens.push_back(str_in);
 
     return tokens;
 }
+
 
 class Item {
 public:
     std::string name;
     float price;
     float wait;
-
-    //Item(std::string in_name, float in_price, float in_wait) : name(in_name), price(in_price), wait(in_wait) {}
     
-    Item(std::string str) {
-
-        std::vector<std::string> arr = split(str, " ");
+    Item(std::vector<std::string> arr) {
 
         name = arr[0];
         price = stof(arr[1]);
@@ -45,16 +45,15 @@ public:
     std::vector<Item> items;
 
 
-    ShopBase(std::string str) {
-        std::vector<std::string> lines_list;
+    ShopBase(std::vector<std::string> lines_list) {
 
-        lines_list = split(str, "\n");
-
-        name = split(lines_list[0], " ")[0];
-        lines_list.erase(std::next(lines_list.begin(), 0), std::next(lines_list.begin(), 1));
+        name = lines_list[0];
 
         for (std::string str : lines_list) {
-            items.emplace_back(str);
+            std::vector<std::string> item_vec = split(str, " ");
+            if (item_vec.size() == 3) {
+                items.push_back(Item(item_vec));
+            }
         }
     }
 
@@ -63,9 +62,9 @@ public:
 
 class CustomShop : public ShopBase {
 public:
-    CustomShop(const std::string& str) : ShopBase(str) {}
+    CustomShop(std::vector<std::string> lines_list) : ShopBase(lines_list) {}
 
-    void wright_mid_price() {
+    float mid_price() {
 
         float mid_price = 0;
         for (Item item : items) {
@@ -73,7 +72,20 @@ public:
         }
 
         mid_price /= items.size();
-        std::cout << mid_price;
+
+        return mid_price;
+    }
+
+    float mid_wait() {
+
+        float mid_wait = 0;
+        for (Item item : items) {
+            mid_wait += item.wait;
+        }
+
+        mid_wait /= items.size();
+
+        return mid_wait;
     }
 };
 
@@ -82,18 +94,29 @@ int main() {
     std::vector<std::string> stors_strs_list;
 
     file.open("stors_file.txt");
-    std::string file_text((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    std::string file_text;
+    std::string line;
+    while (std::getline(file, line)) {
+        file_text += (line + "|");
+    }
+
     file.close();
 
-    stors_strs_list = split(file_text, "Store");
+    stors_strs_list = split(file_text, "Store ");
 
     std::vector<CustomShop> stors;
     for (std::string str : stors_strs_list) {
-        stors.emplace_back(str);
+        std::vector<std::string> lines_list = split(str, "|");
+        if (lines_list.size() >= 2) {
+            stors.push_back(CustomShop(lines_list));
+        }
     }
 
     for (CustomShop shop : stors) {
-        shop.wright_mid_price();
+
+        std::cout << "for a shop " << shop.name << " awerege price is " << shop.mid_price() << " and awerege weight is " << shop.mid_price() << '\n';
+        
     }
 
     return 0;
