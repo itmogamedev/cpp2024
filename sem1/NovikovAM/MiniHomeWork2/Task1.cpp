c9199bd3d1b78c60f871bb67b40c78f63d9d8e7b
Aleksander <farkak@bk.ru>
2024-11-13 19:43:57 +0300

    MiniHW2

--- sem1/NovikovAM/MiniHomeWork2/Task1.cpp
+++ sem1/NovikovAM/MiniHomeWork2/Task1.cpp
@@ -1,70 +1,43 @@
 #include <iostream>
 
-
-std::string TransformDecToHex(int Dec) {
-    std::string hex = "0123456789ABCDEF";
-    std::string CodeHex;
-    char HexChar;
-    while (Dec != 0) {
-      if (Dec % 16 <= 15 and Dec % 16 > 9) {
-          CodeHex = hex[(Dec % 16)] + CodeHex;
-          Dec = Dec / 16;
-      }
-      else {
-        CodeHex = std::to_string(Dec % 16) + CodeHex;
-        Dec = Dec / 16;
-        }
+std::string RevertToHexToOct(int Dec, std::string tableSym) {
+    std::string HexOctNum;
+    while (Dec > 0) {
+        HexOctNum = tableSym[Dec%tableSym.length()] + HexOctNum;
+        Dec = Dec /tableSym.length();
     }
-    CodeHex = "0x" + CodeHex;
-    return CodeHex;
+    return HexOctNum;
 }
 
-std::string TransformDecToOct(int Dec) {
-    std::string oct = "01234567";
-    std::string CodeHex;
-    while (Dec != 0) {
-        if (Dec % 8 <= 7) {
-            CodeHex = oct[(Dec % 8)] + CodeHex;
-            Dec = Dec / 8;
-        }
-        else {
-            CodeHex = std::to_string(Dec % 8) + CodeHex;
-            Dec = Dec / 8;
-        }
-        }
-    CodeHex = "0" + CodeHex;
-    return CodeHex;
-}
 
 int main() {
-    int Dec;
-    int Choise;
-    bool work = true;
+    std::string OctTable = "01234567";
+    std::string HexTable = "0123456789ABCDEF";
 
-    while (work) {
-    std::cout << "Enter your choise transform dec number (1 - 16, 2 - 8, 0 - Exit): ";
-    std::cin >> Choise;
-    switch (Choise) {
-        case 1:
-            std::cout << "Enter your Decimal Number: ";
-            std::cin >> Dec;
-            std::cout << "Oct number: " << TransformDecToHex(Dec) << std::endl;
-        break;
-        case 2:
-          std::cout << "Enter your Decimal Number: ";
-          std::cin >> Dec;
-          std::cout << "Oct number: " << TransformDecToOct(Dec) << std::endl;
-          break;
-        case 0:
-          work = false;
-          break;
-        default:
-            std::cout << "Invalid Choice!" << std::endl;
+    bool Exit = true;
+
+    int Choiсe;
+    int Dec;
+
+    while (Exit){
+        std::cout << "Enter choice revert(1 - Hex, 2 - Oct, 0 - Exit): ";
+        std::cin >> Choiсe;
+        std::cout << "Enter your Dec number: " << std::endl;
+        std::cin >> Dec;
+        switch (Choiсe) {
+            case 1:
+                std::cout<< "Your Hex number: " << RevertToHexToOct(Dec,HexTable) << std::endl;
+
             break;
-    }
+            case 2:
+                std::cout<< "Your Oct number: " << RevertToHexToOct(Dec,OctTable) << std::endl;
+            break;
+            case 0:
+                Exit = false;
+                break;
+            default:
+                std::cout << "Invalid Choice";
+            break;
+        }
     }
 }
-
-
-
-
---
+++ sem1/NovikovAM/MiniHomeWork2/Task2.cpp
@@ -0,0 +1,38 @@
+﻿// Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+class Task2
+{
+    int InputNum;
+    int supposed1;
+public:
+    Task2(int suppoused1) {
+        supposed1 = suppoused1;
+
+    }
+
+    int Sirakuz(int supposed1) {
+        while (supposed1 != 1 and supposed1 != 0) {
+            if (supposed1 % 2 == 0) {
+                supposed1 = supposed1 / 2;
+            }
+            else {
+                supposed1 = ((supposed1 * 3) + 1) / 2;
+            };
+        }
+        return supposed1;
+    }
+
+};
+
+int main() {
+    int input;
+    std::cout << "Enter number: ";
+    std::cin >> input;
+
+    Task2 dsda(input);
+    std::cout << dsda.Sirakuz(input);
+}
