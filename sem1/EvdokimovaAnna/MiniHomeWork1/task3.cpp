e6cad0268f9385d181b3b0de62d27b866cc41b57
Anna Evdokimova <ivanevdokimov802@gmail.com>
2024-12-19 23:03:12 +0300

    Update task3.cpp

--- sem1/EvdokimovaAnna/MiniHomeWork1/task3.cpp
+++ sem1/EvdokimovaAnna/MiniHomeWork1/task3.cpp
@@ -4,14 +4,18 @@ using std::cin;
 
 int main()
 {
-	int a = 0; int b = 1; int c; int x;
+	int a = 0; 
+	int b = 1; 
+	int c; 
+	int x;
 	cin >> x;
 	cout << 1 << "";
-	for (int i = 0; i < x - 1; ++i) {
+	for (int i = 0; i < x - 1; ++i) 
+	{
 		c = a + b;
 		a = b;
 		b = c;
-		cout << c << "";
+		cout << c;
 	}
-
+	return 0;
 }
