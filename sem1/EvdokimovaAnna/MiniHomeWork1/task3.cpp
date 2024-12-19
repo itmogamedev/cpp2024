ecdb7120dc46fdedce430be734a90c66849a509f
Anna Evdokimova <ivanevdokimov802@gmail.com>
2024-12-19 23:27:21 +0300

    Update task3.cpp

--- sem1/EvdokimovaAnna/MiniHomeWork1/task3.cpp
+++ sem1/EvdokimovaAnna/MiniHomeWork1/task3.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
+
 using std::cout;
 using std::cin;
+using std::endl;
 
-int main()
+int main() 
 {
-	int a = 0; 
-	int b = 1; 
-	int c; 
-	int x;
-	cin >> x;
-	cout << 1 << "";
-	for (int i = 0; i < x - 1; ++i) 
+    int N;
+    cout << "Enter the number of Fibonacci elements (N): ";
+    cin >> N;
+
+    long long first = 0, second = 1;
+
+    cout << "A Fibonacci series of " << N << " elements: " << endl;
+
+    for (int i = 0; i < N; ++i) 
+    {
+        if (i == 0) 
 	{
-		c = a + b;
-		a = b;
-		b = c;
-		cout << c;
-	}
-	return 0;
+            cout << first << " "; 
+        } 
+	else if (i == 1) 
+	{
+            cout << second << " "; 
+        }
+	else 
+	{
+            long long next = first + second; 
+            cout << next << " "; 
+            first = second; 
+            second = next; 
+        }
+    }
+    return 0; 
 }
