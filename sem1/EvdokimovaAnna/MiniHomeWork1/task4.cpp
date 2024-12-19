8d32c59cf23d7d9af8ac34ff030464a5a16f9663
Anna Evdokimova <ivanevdokimov802@gmail.com>
2024-12-19 23:48:09 +0300

    Update task4.cpp

--- sem1/EvdokimovaAnna/MiniHomeWork1/task4.cpp
+++ sem1/EvdokimovaAnna/MiniHomeWork1/task4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using std::cout;
 using std::cin;
 using std::endl;
@@ -6,11 +7,14 @@ using std::endl;
 int main()
 {
 	int a, b = 0;
+	cout << "Enter a number: ";
 	cin >> a;
-	while (a > 0) {
+	while (a > 0) 
+	{
 		b *= 10;
 		b += (a % 10);
 		a /=10;
 	}
 	cout << b;
+	return 0;
 }
