dc6e5c2f42b3e02493f94e07e55ece73dac0e354
Anna Evdokimova <ivanevdokimov802@gmail.com>
2024-12-19 23:41:55 +0300

    Update task2.cpp

--- sem1/EvdokimovaAnna/MiniHomeWork1/task2.cpp
+++ sem1/EvdokimovaAnna/MiniHomeWork1/task2.cpp
@@ -8,8 +8,15 @@ int main()
 	int a;
 	int b;
 	int h;
+	
+	cout << "Enter the larger base: ";
 	cin >> a;
+	cout << "\nEnter the smaller base: ";
 	cin >> b;
+	cout << "\nEnter the height: ";
 	cin >> h;
+	cout << "\nArea of the trapezoid:";
 	cout << (a + b) / 2 * h;
+
+	return 0;
 }
