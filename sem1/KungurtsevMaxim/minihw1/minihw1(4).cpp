64a5420bed98f313bf8cc89b8e53e2a710bc0a43
maxkungurtsev <78636999+maxkungurtsev@users.noreply.github.com>
2024-12-20 17:26:54 +0300

    Update minihw1(4).cpp

--- sem1/KungurtsevMaxim/minihw1/minihw1(4).cpp
+++ sem1/KungurtsevMaxim/minihw1/minihw1(4).cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <cmath>
-//задание 4
+//task 4
 int main() {
 	setlocale(LC_ALL, "RU");
-	std::cout << "Введите число, которое нужно отзеркалить:";
+	std::cout << "input a number to reverse";
 	int ToCountLenght;
 	std::cin >> ToCountLenght;
 	int ToReverse = ToCountLenght;
@@ -13,8 +13,9 @@ int main() {
 	} while (ToCountLenght /= 10);
 	int  result = 0;
 	for (int i = 0; i < len; i++) {
+		std::cout << ToReverse / int(pow(10, len - i - 1)) % 10 << "\n";
 		result += ToReverse / int(pow(10, len - i - 1)) % 10 * pow(10, i);
 	}
-	std::cout << result << "\n";
+	std::cout << "reversed number is:"<<result << "\n";
 	return 0;
 }
