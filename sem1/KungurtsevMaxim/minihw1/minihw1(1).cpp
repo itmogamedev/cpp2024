0c2cc94112f9926721913cc323c4d40ad9a545e1
maxkungurtsev <78636999+maxkungurtsev@users.noreply.github.com>
2024-12-20 16:55:58 +0300

    minihw1

---
+++ sem1/KungurtsevMaxim/minihw1/minihw1(1).cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+//задание 1
+int main() {
+	setlocale(LC_ALL, "RU");
+	std::cout << "Введите длинну стороны квадрата";
+	float side;
+	std::cin >> side;
+	std::cout << side * side;
+	return 0;
+}
