5e8f418f7d219c6a177c8ebfa06d0487c87ca6df
Kamalova Julia <94254451+juuunnii@users.noreply.github.com>
2024-12-24 23:44:49 +0300

    Update task1.cpp

--- sem1/Kamalova Julia/MiniHomeWork2/task1.cpp
+++ sem1/Kamalova Julia/MiniHomeWork2/task1.cpp
@@ -1,46 +1,46 @@
 #include <iostream> 
 #include <string> 
  
-using namespace std; 
- 
-string decToOct(int dec) { 
-  string oct = ""; 
-  if (dec == 0) { 
-    return "0";  
-  } 
-  while (dec > 0) { 
-    int remainder = dec % 8;  
-    oct = to_string(remainder) + oct;  
-    dec /= 8;  
-  } 
-  return oct; 
+std::string decToOct(int dec) { 
+    std::string oct = ""; 
+    if (dec == 0) { 
+        return "0"; 
+    } 
+    while (dec > 0) { 
+        int remainder = dec % 8; 
+        oct = std::to_string(remainder) + oct;  
+        dec /= 8; 
+    } 
+    return oct; 
 } 
  
-string decToHex(int dec) { 
-  string hex = ""; 
-  if (dec == 0) { 
-    return "0";  
-  } 
-  while (dec > 0) { 
-    int remainder = dec % 16;  
-    if (remainder < 10) {  
-      hex = to_string(remainder) + hex; 
-    } else {   
-      char hexDigit = 'A' + (remainder - 10);  
-      hex = hexDigit + hex;  
+std::string decToHex(int dec) { 
+    std::string hex = ""; 
+    if (dec == 0) { 
+        return "0"; 
     } 
-    dec /= 16;  
-  } 
-  return hex; 
+    while (dec > 0) { 
+        int remainder = dec % 16; 
+        if (remainder < 10) { 
+            hex = std::to_string(remainder) + hex;  
+        } 
+        else { 
+            char hexDigit = 'A' + (remainder - 10); 
+            hex = hexDigit + hex; 
+        } 
+        dec /= 16; 
+    } 
+    return hex; 
 } 
  
 int main() { 
-  int decNum; 
-  cout << "Десятичное число: "; 
-  cin >> decNum; 
+    setlocale(LC_ALL, "RUS"); 
+    int decNum; 
+    std::cout << "Десятичное число: "; 
+    std::cin >> decNum; 
  
-  cout << "Восьмеричное число: " << decToOct(decNum) << endl; 
-  cout << "Шестнадцатеричное число: " << decToHex(decNum) << endl; 
+    std::cout << "Восьмеричное число: " << decToOct(decNum) << std::endl; 
+    std::cout << "Шестнадцатеричное число: " << decToHex(decNum) << std::endl; 
  
-  return 0; 
+    return 0; 
 }
