3df22b5bf9fa1a93233ffec50f5b0709a01e158d
Anna Ushnova <anna.ushnova@gmai.com>
2024-12-24 23:33:32 +0300

    убрала namespace

--- sem1/UshnovaAI/MiniHomeWork2/task1.cpp
+++ sem1/UshnovaAI/MiniHomeWork2/task1.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
-    while(1){
-        cout<<"Select octal and hexadecimal number system\nWrite 8 or 16\n";
-        int numSys,num;
-        cin>>numSys;
-        if ((numSys==16) or (numSys==8)){
-            cout<<"Enter your number:\n";
-            cin>>num;
-            switch(numSys){
-                case 16:
-                cout<<"Hexadecimal number: "<<hex<<num<<endl;
+    while (1) {
+        std::cout << "Select octal and hexadecimal number system\nWrite 8 or 16\n";
+        int numSys, num;
+        std::cin >> numSys;
+        if ((numSys == 16) or (numSys == 8)) {
+            std::cout << "Enter your number:\n";
+            std::cin >> num;
+            switch (numSys) {
+            case 16:
+                std::cout << "Hexadecimal number: " << std::hex << num << std::endl;
                 break;
-                case 8:
-                cout<<"Octal number: "<<oct<<num<<endl;
+            case 8:
+                std::cout << "Octal number: " << std::oct << num << std::endl;
                 break;
             }
-        }else{
-        cout<< "Invalid input\n";
         }
-        
+        else {
+            std::cout << "Invalid input\n";
+        }
+
     }
     return 0;
 }
--- sem1/UshnovaAI/MiniHomeWork2/task2.cpp
+++ sem1/UshnovaAI/MiniHomeWork2/task2.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
-    while(1){
-        cout<<"Enter any natural number\n";
+    while (1) {
+        std::cout << "Enter any natural number\n";
         int num;
-        cin>>num;
-        while (num>1){
-            if (num%2==0){
-                num=num/2;
-                cout<<num<<endl;
-            } else if (num%2==1){
-                num=(num*3+1)/2;
-                cout<<num<<endl;
-            }  
+        std::cin >> num;
+        while (num > 1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+                std::cout << num << std::endl;
+            }
+            else if (num % 2 == 1) {
+                num = (num * 3 + 1) / 2;
+                std::cout << num << std::endl;
+            }
         }
     }
     return 0;
