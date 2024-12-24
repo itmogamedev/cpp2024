d85733854d3386be15b0fc387df1c07dae4aca7b
Mooruu <145343154+Mooruu@users.noreply.github.com>
2024-12-24 23:57:20 +0300

    Updated the path of Mini_Homework1

--- sem1/Scherbakov Marat/Fibonachi.cpp
+++ sem1/Scherbakov Marat/Fibonachi.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    int A = 0, B = 0, C = 1, Amonut_of_N;
-    cout << "Введите кол-во элементов:" << endl;
-    cin >> Amonut_of_N;
-    cout << C << " ";
-    for (int i = 0; i < Amonut_of_N - 1; ++i) {
-        B = A + C;
-        A = C;
-        C = B;
-        cout << B << " "; //Я не понял, как вывести отдельно ноль в начале(
+    int numberOfElements;
+    int firstNumber = 0;
+    int secondNumber = 1;
+    int currentNumber;
+    while (true)
+    {
+        std::cout << "Введите желаемое число элементов ряда: ";
+        std::cin >> numberOfElements;
+
+        if (numberOfElements <= 0)
+            std::cout << "Неверное число элементов:(" << std::endl;
+        else
+        {
+            std::cout << "Ваш ряд: " << 1 << " ";
+            for (int x = 0; x < numberOfElements - 1; ++x)
+            {
+                currentNumber = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = currentNumber;
+                std::cout << currentNumber << " ";
+            }
+            break;
+        }
     }
 }
