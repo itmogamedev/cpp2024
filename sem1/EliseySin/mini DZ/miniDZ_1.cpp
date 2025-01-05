67bd6ea704e9ff0abbae6e42f123f612de29e2c6
Elisey Sin <57361980+Nionchik@users.noreply.github.com>
2025-01-05 18:01:53 +0300

    Update miniDZ_1.cpp

--- sem1/EliseySin/mini DZ/miniDZ_1.cpp
+++ sem1/EliseySin/mini DZ/miniDZ_1.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include <iomanip>
 
-int main() {
-    int decimal;
+std::string toOctal(int number) 
+{
+    std::string octal;
+    while (number > 0) 
+    {
+        octal = std::to_string(number % 8) + octal;
+        number /= 8;
+    }
+    return octal.empty() ? "0" : octal;
+}
 
-    std::cout << "Введите десятичное число: ";
-    std::cin >> decimal;
+std::string toHexadecimal(int number) 
+{
+    std::string hexadecimal;
+    const char hexDigits[] = "0123456789ABCDEF";
+    while (number > 0) 
+    {
+        hexadecimal = hexDigits[number % 16] + hexadecimal;
+        number /= 16;
+    }
+    return hexadecimal.empty() ? "0" : hexadecimal;
+}
 
-    std::cout << "Введенное число в шестнадцатиричной системе: " 
-              << std::hex << std::uppercase << decimal << std::endl;
+int main() 
+{
+    int number;
 
-    std::cout << "Введенное число в восьмиричной системе: " 
-              << std::oct << decimal << std::endl;
+    std::cout << "Введите целое число: ";
+    std::cin >> number;
 
+    std::string octal = toOctal(number);
+    std::string hexadecimal = toHexadecimal(number);
+
+    std::cout << "Введенное число в восьмиричной системе: : " << octal << std::endl;
+    std::cout << "Введенное число в шестнадцатиричной системе: " << hexadecimal << std::endl;
+
+    return 0;
 }
