37d1fdf0694ec24167c63d10420f07de6c574176
Elisey Sin <57361980+Nionchik@users.noreply.github.com>
2025-01-05 20:22:55 +0300

    Update dz_2.cpp

--- sem1/EliseySin/DZ_1/dz_2.cpp
+++ sem1/EliseySin/DZ_1/dz_2.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 
-int main()
+
+int calculateTrapezoidArea(int bst, int mst, int vt) 
+{
+    return ((bst + mst) * vt) / 2;
+}
+
+int main() 
 {
     int bst;
     int mst;
     int vt;
-    std::cout<<"Введите большую сторону трапеции: ";
-    std::cin>> bst;
-    std::cout<<"Введите меньшую сторону трапеции: ";
-    std::cin>> mst;
-    std::cout<<"Введите высоту трапеции: ";
-    std::cin>> vt;
     
-    int pt = (bst*mst)/2*vt;
-    std::cout<< pt;
+    std::cout << "Введите большую сторону трапеции: ";
+    std::cin >> bst;
+    std::cout << "Введите меньшую сторону трапеции: ";
+    std::cin >> mst;
+    std::cout << "Введите высоту трапеции: ";
+    std::cin >> vt;
+    
+    int pt = calculateTrapezoidArea(bst, mst, vt);
+    std::cout << "Площадь трапеции: " << pt;
     
     return 0;
 }
