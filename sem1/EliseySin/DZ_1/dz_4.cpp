94ed21a989e21428c88162293715e4aee3b818bb
Elisey Sin <57361980+Nionchik@users.noreply.github.com>
2025-01-05 20:26:42 +0300

    Update dz_4.cpp

--- sem1/EliseySin/DZ_1/dz_4.cpp
+++ sem1/EliseySin/DZ_1/dz_4.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 
+void HypothesisSyracuse(int n) 
+{
+    if (n <= 0) 
+    {
+        std::cout << "Ошибка: Введите положительное целое число!" << std::endl;
+        return;
+    }
+    
+    std::cout << "Последовательность для числа " << n << ": ";
+    while (n != 1) 
+    {
+        std::cout << n << " ";
+        if (n % 2 == 0) 
+        {
+            n /= 2;
+        } 
+        else 
+        {
+            n = (3 * n + 1) / 2;
+        }
+    }
+    std::cout << n << std::endl;
+    std::cout << "Гипотеза подтверждена!" << std::endl;
+}
+
 int main() 
 {
     int number;
-    int rnumber = 0;
-
-    std::cout << "Введите любое число, чтобы его развернуть: ";
+    
+    std::cout << "Введите положительное целое число: ";
     std::cin >> number;
 
-    while (number != 0)
-    {
-        rnumber *= 10;
-        rnumber += chislo % 10;
-        number /= 10;
-    }
-
-    std::cout << rnumber;
-
+    HypothesisSyracuse(number);
+    return 0;
 }
